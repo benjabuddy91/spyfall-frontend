86f01b7cd6e3f287a71c1b5239acf141a18dd33a
Benjamin Zhiwei NG <benjaminngz@dbs.com>
2017-09-06 12:15:25 +0800

    Generate game-console and game-init component

---
+++ src/app/game-console/game-init/game-init.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-init',
+  templateUrl: './game-init.component.html',
+  styleUrls: ['./game-init.component.css']
+})
+export class GameInitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
