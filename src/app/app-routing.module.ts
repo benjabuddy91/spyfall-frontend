ce42b2d5ff1277c0c72436f1d88a414fedb09f10
Benjamin Zhiwei NG <benjaminngz@dbs.com>
2017-09-06 15:18:12 +0800

    Add routerLinks to game-init component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { GameConsoleComponent } from './game-console/game-console.component';
 import { GameInitComponent } from './game-console/game-init/game-init.component';
 import { GameNewComponent } from './game-console/game-new/game-new.component';
+import { GameJoinComponent } from './game-console/game-join/game-join.component';
 
 const appRoutes: Routes = [
   { path: '', component: GameConsoleComponent, children: [
     { path: '', component: GameInitComponent },
-    { path: 'new', component: GameNewComponent }
+    { path: 'new', component: GameNewComponent },
+    { path: 'join', component: GameJoinComponent }
   ] }
 ];
 
