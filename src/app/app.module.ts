3f7e7352a7811ca34ad8f37124d4dc1405a3bee4
Benjamin Zhiwei NG <benjaminngz@dbs.com>
2017-09-06 12:14:27 +0800

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GameConsoleComponent } from './game-console/game-console.component';
+import { GameInitComponent } from './game-console/game-init/game-init.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameConsoleComponent,
+    GameInitComponent
   ],
   imports: [
     BrowserModule
